0894de80275a03f3062caece8d638d3f07d5e7b3
grasshopper001 <grasshopper001@sjtu.edu.cn>
2019-01-07 14:38:28 +0800

    Create MMM-TestNotification.js
    first version

---
+++ MMM-TestNotification.js
@@ -0,0 +1,68 @@
+
+
+
+Module.register("MMM-TestNotifications",{
+    defaults:{
+        //to be used
+        showSchedule:false
+    },
+    start:function(){
+        //60s counter
+        this.count=0;
+        //saved for receiving other notificaiton
+        this.info=0;
+    },
+    
+    getDom:function(){},
+    
+    
+    
+    notificationReceived:function(notification,payload,sender){
+        var self=this;
+        switch(notification){
+            case "DOM_OBJECTS_CREATED":
+                this.backToIndex();
+                 setInterval(()=>{
+                     if(this.count>0){this.count--;}
+                     if(this.showSchedule===true){
+                         self.shutOtherModule();
+                         this.count=60;
+                         this.showSchedule=false;
+                         this.updateDom();
+                     }
+                     if(this.count===0){
+                         self.backToIndex();
+                         this.updateDom();
+                     }
+                 },1000) 
+                  break;
+            case "TEST_NOTI":
+                 this.info=payload;
+                 break;
+            case "showSchedule":
+                 this.showSchedule=payload;
+            default:
+                break;
+            }
+    },
+    shutOtherModule:function(){
+        MM.getModules().exceptModule(this).enumerate(function(module){
+            if(module.name!=="MMM-ImagesPhotos"){
+                module.hide();
+            }
+            else{
+                module.show();
+            }
+        })
+    }, 
+    backToIndex:function(){
+        MM.getModules().exceptModule(this).enumerate(function(module){
+            if(module.name!=="MMM-ImagesPhotos"&&module.name!=="MMM-MQTT"){
+                module.show();
+            }
+            else{
+                module.hide();
+            }
+        })
+    }, 
+})   
